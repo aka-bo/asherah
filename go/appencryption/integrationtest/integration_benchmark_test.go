e9a226957b98d9070ecb7c844eb19ce701647eaa
Bo Thompson <bo@godaddy.com>
2023-08-30 11:26:57 -0700

    new delayedMetastore for simulating real world latencies

--- go/appencryption/integrationtest/integration_benchmark_test.go
+++ go/appencryption/integrationtest/integration_benchmark_test.go
@@ -27,15 +27,72 @@ const (
 )
 
 var (
-	c      = aead.NewAES256GCM()
-	config = &appencryption.Config{
-		Policy:  appencryption.NewCryptoPolicy(),
+	c         = aead.NewAES256GCM()
+	metastore = newDelayedMetastore(10*time.Millisecond, 5*time.Millisecond)
+	zipfSeed  = time.Now().UnixNano()
+)
+
+func getConfig() *appencryption.Config {
+	policy := appencryption.NewCryptoPolicy(
+		appencryption.WithRevokeCheckInterval(10 * time.Second),
+	)
+
+	policy.CreateDatePrecision = time.Second
+
+	return &appencryption.Config{
+		Policy:  policy,
 		Product: product,
 		Service: service,
 	}
-	metastore = persistence.NewMemoryMetastore()
-	zipfSeed  = time.Now().UnixNano()
-)
+}
+
+type delayedMetastore struct {
+	m      *persistence.MemoryMetastore
+	delay  time.Duration
+	jitter time.Duration
+}
+
+func newDelayedMetastore(delay time.Duration, jitter time.Duration) *delayedMetastore {
+	return &delayedMetastore{
+		m:      persistence.NewMemoryMetastore(),
+		delay:  delay,
+		jitter: jitter,
+	}
+}
+
+func (d *delayedMetastore) delayWithJitter() {
+	ch := make(chan int)
+	go func() {
+		randJitter := int64(0)
+		if d.jitter > 0 {
+			randJitter = rand.Int63n(int64(d.jitter))
+		}
+
+		time.Sleep(d.delay + time.Duration(randJitter))
+
+		ch <- 1
+	}()
+
+	<-ch
+}
+
+func (d *delayedMetastore) Load(ctx context.Context, keyID string, created int64) (*appencryption.EnvelopeKeyRecord, error) {
+	d.delayWithJitter()
+
+	return d.m.Load(ctx, keyID, created)
+}
+
+func (d *delayedMetastore) LoadLatest(ctx context.Context, keyID string) (*appencryption.EnvelopeKeyRecord, error) {
+	d.delayWithJitter()
+
+	return d.m.LoadLatest(ctx, keyID)
+}
+
+func (d *delayedMetastore) Store(ctx context.Context, keyID string, created int64, envelope *appencryption.EnvelopeKeyRecord) (bool, error) {
+	d.delayWithJitter()
+
+	return d.m.Store(ctx, keyID, created, envelope)
+}
 
 func TestMain(m *testing.M) {
 	log.Printf("random seed: %d\n", zipfSeed)
@@ -52,7 +109,7 @@ func Benchmark_Encrypt(b *testing.B) {
 	defer km.Close()
 
 	factory := appencryption.NewSessionFactory(
-		config,
+		getConfig(),
 		metastore,
 		km,
 		c,
@@ -71,13 +128,15 @@ func Benchmark_Encrypt(b *testing.B) {
 
 	ctx := context.Background()
 
-	for i := 0; i < b.N; i++ {
-		bytes := randomBytes[i]
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			bytes := randomBytes[b.N-1]
 
-		if _, err := sess.Encrypt(ctx, bytes); err != nil {
-			b.Error(err)
+			if _, err := sess.Encrypt(ctx, bytes); err != nil {
+				b.Error(err)
+			}
 		}
-	}
+	})
 }
 
 func Benchmark_EncryptDecrypt_MultiFactorySamePartition(b *testing.B) {
@@ -89,7 +148,7 @@ func Benchmark_EncryptDecrypt_MultiFactorySamePartition(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			factory := appencryption.NewSessionFactory(
-				config,
+				getConfig(),
 				metastore,
 				km,
 				c,
@@ -121,9 +180,9 @@ func Benchmark_EncryptDecrypt_MultiFactoryUniquePartition(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		zipf := newZipf()
 
-		for i := 0; i < b.N && pb.Next(); i++ {
+		for pb.Next() {
 			factory := appencryption.NewSessionFactory(
-				config,
+				getConfig(),
 				metastore,
 				km,
 				c,
@@ -152,10 +211,8 @@ func Benchmark_EncryptDecrypt_SameFactoryUniquePartition(b *testing.B) {
 
 	defer km.Close()
 
-	metastore := persistence.NewMemoryMetastore()
-
 	factory := appencryption.NewSessionFactory(
-		config,
+		getConfig(),
 		metastore,
 		km,
 		c,
@@ -185,13 +242,15 @@ func Benchmark_EncryptDecrypt_SameFactoryUniquePartition(b *testing.B) {
 	})
 }
 
+// newZipf returns a function that returns a random uint64 value that follows a Zipf distribution.
+// A Zipf distribution is used to simulate a real-world scenario where a small number of keys are
+// used more frequently than others.
 func newZipf() func() uint64 {
-	cap := uint64(appencryption.DefaultSessionCacheMaxSize)
 	zipfS := 1.0001
 	v := 10.0
-	n := cap * 32
+	n := appencryption.DefaultSessionCacheMaxSize * 32
 
-	z := rand.NewZipf(rand.New(rand.NewSource(zipfSeed)), zipfS, v, n)
+	z := rand.NewZipf(rand.New(rand.NewSource(zipfSeed)), zipfS, v, uint64(n))
 
 	return z.Uint64
 }
@@ -202,15 +261,13 @@ func Benchmark_EncryptDecrypt_SameFactoryUniquePartition_WithSessionCache(b *tes
 
 	defer km.Close()
 
+	config := getConfig()
+	config.Policy.CacheSessions = true
+	config.Policy.SessionCacheDuration = 20 * time.Second
+	config.Policy.SessionCacheMaxSize = appencryption.DefaultSessionCacheMaxSize
+
 	factory := appencryption.NewSessionFactory(
-		&appencryption.Config{
-			Policy: appencryption.NewCryptoPolicy(
-				appencryption.WithSessionCache(),
-				appencryption.WithSessionCacheMaxSize(1000),
-			),
-			Product: product,
-			Service: service,
-		},
+		config,
 		metastore,
 		km,
 		c,
@@ -222,24 +279,21 @@ func Benchmark_EncryptDecrypt_SameFactoryUniquePartition_WithSessionCache(b *tes
 		zipf := newZipf()
 
 		for pb.Next() {
-			func() {
-				partition := fmt.Sprintf("%d", zipf())
+			partition := fmt.Sprintf("%d", zipf())
+			randomBytes := internal.GetRandBytes(payloadSizeBytes)
 
-				randomBytes := internal.GetRandBytes(payloadSizeBytes)
+			sess, _ := factory.GetSession(partition)
+			ctx := context.Background()
 
-				sess, _ := factory.GetSession(partition)
-				ctx := context.Background()
+			drr, err := sess.Encrypt(ctx, randomBytes)
+			if err != nil {
+				b.Error(err)
+			}
 
-				drr, err := sess.Encrypt(ctx, randomBytes)
-				if err != nil {
-					b.Error(err)
-				}
+			data, _ := sess.Decrypt(ctx, *drr)
+			assert.Equal(b, randomBytes, data)
 
-				data, _ := sess.Decrypt(ctx, *drr)
-				assert.Equal(b, randomBytes, data)
-
-				sess.Close()
-			}()
+			sess.Close()
 		}
 	})
 }
@@ -251,7 +305,7 @@ func Benchmark_EncryptDecrypt_SameFactorySamePartition(b *testing.B) {
 	defer km.Close()
 
 	factory := appencryption.NewSessionFactory(
-		config,
+		getConfig(),
 		metastore,
 		km,
 		c,
@@ -284,16 +338,13 @@ func Benchmark_EncryptDecrypt_SameFactorySamePartition_WithSessionCache(b *testi
 
 	defer km.Close()
 
-	conf := &appencryption.Config{
-		Policy: appencryption.NewCryptoPolicy(
-			appencryption.WithSessionCache(),
-		),
-		Product: product,
-		Service: service,
-	}
+	config := getConfig()
+	config.Policy.CacheSessions = true
+	config.Policy.SessionCacheDuration = 20 * time.Second
+	config.Policy.SessionCacheMaxSize = 10
 
 	factory := appencryption.NewSessionFactory(
-		conf,
+		config,
 		metastore,
 		km,
 		c,
